0a4aedab35be43c836c9dfcefe360f5481c2474b
Marcus Boay <marcusboay@hotmail.com>
2022-04-27 19:55:47 -0600

    make player move on screen

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use bevy::{
+    prelude::*,
+    math::const_vec3
+};
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    Overworld,
+    Exit,
+}
+
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+// TODO: replace with map image
+const TEMP_BACKGROUND_COLOR: Color = Color::WHITE;
+// TODO: replace with player image
+const TEMP_PLAYER_COLOR: Color = Color::PINK;
+const PLAYER_SIZE: Vec3 = const_vec3!([64.0, 64.0, 0.0]);
+const PLAYER_SPEED: f32 = 640.0;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapidFantasyPlugin)
+        .run();
+}
+
+pub struct RapidFantasyPlugin;
+
+impl Plugin for RapidFantasyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup)
+            .add_system_set(
+                SystemSet::new()
+                    .with_system(move_player)
+            )
+            // .add_system(move_player);
+            ;
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Cameras
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    // Player
+    commands.spawn()
+        .insert(Player)
+        .insert_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 50.0, 0.0),
+                scale: PLAYER_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: TEMP_PLAYER_COLOR,
+                ..default()
+            },
+            ..default()
+        });
+}
+
+#[derive(Component)]
+struct Player;
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>
+) {
+    let mut player_transform = query.single_mut();
+    let mut direction_horizontal = 0.0;
+    let mut direction_vertical = 0.0;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction_horizontal -= 1.0;
+    } else if keyboard_input.pressed(KeyCode::Right) {
+        direction_horizontal += 1.0;
+    } else if keyboard_input.pressed(KeyCode::Up) {
+        direction_vertical += 1.0;
+    } else if keyboard_input.pressed(KeyCode::Down) {
+        direction_vertical -= 1.0;
+    }
+
+    let new_player_position_x = player_transform.translation.x + direction_horizontal * PLAYER_SPEED * TIME_STEP;
+    let new_player_position_y = player_transform.translation.y + direction_vertical * PLAYER_SPEED * TIME_STEP;
+
+    // TODO: clamp within map area
+    player_transform.translation.x = new_player_position_x;
+    player_transform.translation.y = new_player_position_y;
+}

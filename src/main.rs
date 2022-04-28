b84e1b5539d60ea0f7306e9a1ae43354dc5110a4
Marcus Boay <marcusboay@hotmail.com>
2022-04-27 20:17:28 -0600

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use bevy::{
-    prelude::*,
-    math::const_vec3,
-    render::render_resource::Texture
-};
+use bevy::{math::const_vec3, prelude::*, render::render_resource::Texture};
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
 enum GameState {
@@ -48,21 +44,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     // Player
     let player_image = asset_server.load("player_down.png");
-    commands.spawn()
-        .insert(Player)
-        .insert_bundle(SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, 50.0, 0.0),
-                scale: PLAYER_SIZE,
-                ..default()
-            },
-            texture: player_image,
-            sprite: Sprite {
-                color: TEMP_PLAYER_COLOR,
-                ..default()
-            },
+    commands.spawn().insert(Player).insert_bundle(SpriteBundle {
+        transform: Transform {
+            translation: Vec3::new(0.0, 50.0, 0.0),
+            scale: PLAYER_SIZE,
             ..default()
-        });
+        },
+        texture: player_image,
+        sprite: Sprite {
+            color: TEMP_PLAYER_COLOR,
+            ..default()
+        },
+        ..default()
+    });
 }
 
 #[derive(Component)]
@@ -70,7 +64,7 @@ struct Player;
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Player>>
+    mut query: Query<&mut Transform, With<Player>>,
 ) {
     let mut player_transform = query.single_mut();
     let mut direction_horizontal = 0.0;
@@ -86,8 +80,10 @@ fn move_player(
         direction_vertical -= 1.0;
     }
 
-    let new_player_position_x = player_transform.translation.x + direction_horizontal * PLAYER_SPEED * TIME_STEP;
-    let new_player_position_y = player_transform.translation.y + direction_vertical * PLAYER_SPEED * TIME_STEP;
+    let new_player_position_x =
+        player_transform.translation.x + direction_horizontal * PLAYER_SPEED * TIME_STEP;
+    let new_player_position_y =
+        player_transform.translation.y + direction_vertical * PLAYER_SPEED * TIME_STEP;
 
     // TODO: clamp within map area
     player_transform.translation.x = new_player_position_x;
@@ -97,21 +93,21 @@ fn move_player(
 fn change_player_image(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<&mut Handle<Image>, With<Player>>,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     let mut player_image = query.single_mut();
-    let new_player_image = 
-        if keyboard_input.pressed(KeyCode::Left) {
-            Some(asset_server.load("player_left.png"))
-        } else if keyboard_input.pressed(KeyCode::Right) {
-            Some(asset_server.load("player_right.png"))
-        } else if keyboard_input.pressed(KeyCode::Up) {
-            Some(asset_server.load("player_up.png"))
-        } else if keyboard_input.pressed(KeyCode::Down) {
-            Some(asset_server.load("player_down.png"))
-        } else { // Don't change sprite if no input.
-            None
-        };
+    let new_player_image = if keyboard_input.pressed(KeyCode::Left) {
+        Some(asset_server.load("player_left.png"))
+    } else if keyboard_input.pressed(KeyCode::Right) {
+        Some(asset_server.load("player_right.png"))
+    } else if keyboard_input.pressed(KeyCode::Up) {
+        Some(asset_server.load("player_up.png"))
+    } else if keyboard_input.pressed(KeyCode::Down) {
+        Some(asset_server.load("player_down.png"))
+    } else {
+        // Don't change sprite if no input.
+        None
+    };
 
     if let Some(new_player_image) = new_player_image {
         *player_image = new_player_image;
